7e04949ed7466927579ce0292cc39dd77fae21d4
Grace-Feng <v-linfen@microsoft.com>
2016-04-20 13:20:04 +0800

    grouped list

---
+++ IdentifyingPropertiesGroupedList/BookList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentifyingPropertiesGroupedList
+{
+    public class BookList : INotifyPropertyChanged
+    {
+        public string _BookAddress;
+
+        public string BookAddress
+        {
+            get { return _BookAddress; }
+            set
+            {
+                _BookAddress = value;
+                OnPropertyChanged("BookAddress");
+            }
+        }
+
+        public BookList(string name)
+        {
+            this.BookAddress = name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
